392435f04c6296b1e7c51fa6f93d7149adb5fa38
Peter Neubauer <neubauer.peter@gmail.com>
2024-03-05 19:11:25 +0100

    simpler parsing, display all props, add turf.js for later overlap detection

--- viz.js
+++ viz.js
@@ -8,57 +8,50 @@ var layerControl = L.control.layers();
 layerControl.addTo(map);
 var bounds = map.getBounds();
 var cells_layers = {};
+var cell_control_layers = [];
 
-var cells = {};
+var cells_by_key = {};
 
-function loadGeoJson(location, layername) {
-    $.getJSON( location, function( data ) {
-        console.log(data);
-        var layer = L.geoJson(data,{
-            style: function (feature) {
-                return {color: feature.properties.color};
-            }
-        }).bindPopup(function (layer) {
-            return "name: "+layer.feature.properties.name;
-        });
-        var group = new L.featureGroup([layer]);
-        layer.addTo(map);
-        bounds = layer.getBounds();
-        map.fitBounds(bounds);
-        layerControl.addOverlay(layer, layername);
-        map.setView(group.getBounds().getCenter());
-        if (data['type'] === 'FeatureCollection') {
-            var layer = L.geoJson(data,{
-            style: function (feature) {
-                return {color: feature.properties.color};
-            }
-        }).bindPopup(function (layer) {
-            return "name: "+layer.feature.properties.name;
-        });
-            data['features'].forEach(feat => {
-                var layer_feature = L.geoJson(feat,{
-                    style: function (feat) {
-                        return {color: feat.properties.color};
+async function loadGeoJson(location, layername) {
+    await fetch(location)
+        .then(response => response.json())
+        .then(data => {
+                console.log(data);
+                var layer = L.geoJson(data,{
+                    style: function (feature) {
+                        return {color: feature.properties.color};
                     }
-                }).bindPopup(function (layer) {
-                    return "name: "+layer.feature.properties.name;
+                })
+                .bindPopup(function (layer) {
+                    console.log(layer.feature.properties);
+                    return JSON.stringify(layer.feature.properties);
                 });
-                loadCell(layer_feature.getBounds().getCenter(), "Cell in "+feat.properties.name);
+                layer.addTo(map);
+                bounds = layer.getBounds();
+                map.fitBounds(bounds);
+                layerControl.addOverlay(layer, layername);
+                map.setView(bounds.getCenter());
+                data['features'].forEach(feat => {
+                    var layer_feature = L.geoJson(feat);
+                    loadCell(layer_feature.getBounds().getCenter(), "Cell in "+feat.properties.name);
 
-            });
-        }
-        // layerControl.addOverlay(cells_layers, "cells");
-
-    });
+                });
+        })
+        .catch(error => {
+            console.error('Error loading GeoJSON data:', error);
+        });
 }
 
-loadGeoJson("data/biotopes/biotope_1.json", "Biotop 1");
-loadGeoJson("data/areas/ustorp_1_6.json", "Fastighet Ustorp 1:6");
+async function getData() {
+    await loadGeoJson("data/biotopes/biotope_1.json", "Biotop 1");
+    await loadGeoJson("data/areas/ustorp_1_6.json", "Fastighet Ustorp 1:6");
+
+}
 
 function loadCell(center, name) {
     var key = S2.latLngToKey(center.lat, center.lng, 16);
-    console.log(key, cells);
-    if(cells[key]) {
+    console.log(key, cells_by_key);
+    if(cells_by_key[key]) {
         console.log("found cell already");
     }
     var corners = S2.S2Cell.FromLatLng(center, 16).getCornerLatLngs();
@@ -83,6 +76,16 @@ function loadCell(center, name) {
                 return "name: "+name+"<br/>key: "+key;
             });
     layer.addTo(map);
+    cell_control_layers.push(layer);
     cells_layers[name] = layer;
+    cells_by_key[key] = [geoJson];
 
 }
+
+getData()
+    .then(() => {
+        const layerGroup = L.layerGroup(cell_control_layers);
+        layerControl.addOverlay(layerGroup, "S2 cells");
+        console.log("done");
+
+    })
